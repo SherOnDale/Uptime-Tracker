ee71060d72dea582473ff81002b6d19556a19cb0
Sherin Binu <sherinbinu@hotmail.com>
2018-09-16 14:44:45 +0530

    Parses payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -12,13 +13,22 @@ const server = http.createServer((req, res) => {
 
     const headers = req.headers;
 
-    res.end(`Hello World\n`);
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', data => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
 
-    console.log(
-        `${method} ${trimmedPath} Query: ${JSON.stringify(
-            queryStringObject
-        )} Headers: ${JSON.stringify(headers)}`
-    );
+        res.end(`Hello World\n`);
+
+        console.log(
+            `${method} ${trimmedPath} Query: ${JSON.stringify(
+                queryStringObject
+            )} Headers: ${JSON.stringify(headers)} Payload: ${buffer}`
+        );
+    });
 });
 
 server.listen(3000, () => console.log('The server is listening on port 3000'));

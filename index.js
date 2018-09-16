9c50fecce24554a6c25aa1e4d8c00df8d2edb78d
Sherin Binu <sherinbinu@hotmail.com>
2018-09-16 14:07:27 +0530

    Determines request method

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const server = http.createServer((req, res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+    const method = req.method.toLowerCase();
+
     res.end(`Hello World\n`);
 
-    console.log('Request is received on path: ' + trimmedPath);
+    console.log(
+        `Request received on path ${trimmedPath} with method ${method}`
+    );
 });
 
 server.listen(3000, () => console.log('The server is listening on port 3000'));
